7b6f7377e263782d29fdf343523a086ccf6aa810
Cyril <cyril.personne@outlook.fr>
2024-07-26 20:31:57 +0200

    customer

--- adapters/primaries/restAdaptater/src/main/java/fr/planandchill/rest/endpoints/CustomerController.java
+++ adapters/primaries/restAdaptater/src/main/java/fr/planandchill/rest/endpoints/CustomerController.java
@@ -2,6 +2,8 @@ package fr.planandchill.rest.endpoints;
 
 import fr.planandchill.models.Customer;
 import fr.planandchill.use.cases.customer.CreateCustomerUE;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
+
+    private final Logger LOG = LoggerFactory.getLogger(CustomerController.class);
 
     private final CreateCustomerUE createCustomerUE;
 
@@ -24,6 +28,7 @@ public class CustomerController {
         try {
             return new ResponseEntity<>(this.createCustomerUE.execute(customer), HttpStatus.OK);
         } catch (Exception e) {
+            LOG.error(e.getMessage());
             e.printStackTrace();
             return new ResponseEntity<>("Erreur technique : Veuillez contacter le support technique", HttpStatus.INTERNAL_SERVER_ERROR);
         }
