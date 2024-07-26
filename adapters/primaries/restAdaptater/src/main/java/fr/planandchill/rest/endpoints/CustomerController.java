d4bf7055d9568ec3114abaaf23103c92821167e0
Cyril <cyril.personne@outlook.fr>
2024-07-26 20:29:54 +0200

    customer

--- adapters/primaries/restAdaptater/src/main/java/fr/planandchill/rest/endpoints/CustomerController.java
+++ adapters/primaries/restAdaptater/src/main/java/fr/planandchill/rest/endpoints/CustomerController.java
@@ -1,8 +1,8 @@
 package fr.planandchill.rest.endpoints;
 
 import fr.planandchill.models.Customer;
-import fr.planandchill.rest.models.ResponseApi;
 import fr.planandchill.use.cases.customer.CreateCustomerUE;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,16 +11,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/customer")
 public class CustomerController {
 
-    private CreateCustomerUE createCustomerUE;
+    private final CreateCustomerUE createCustomerUE;
+
+    @Autowired
+    public CustomerController(CreateCustomerUE createCustomerUE) {
+        this.createCustomerUE = createCustomerUE;
+    }
 
     @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/save")
     public ResponseEntity<?> save(@RequestBody Customer customer) {
         try {
-            return new ResponseEntity<>(new ResponseApi<>(this.createCustomerUE.execute(customer), HttpStatus.OK));
+            return new ResponseEntity<>(this.createCustomerUE.execute(customer), HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>(new ResponseApi<>("Erreur technique : Veuillez contacter le support technique"), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("Erreur technique : Veuillez contacter le support technique", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- core/src/main/java/fr/planandchill/use/cases/customer/CreateCustomerUE.java
+++ core/src/main/java/fr/planandchill/use/cases/customer/CreateCustomerUE.java
@@ -11,6 +11,8 @@ import java.net.UnknownHostException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,6 +46,7 @@ public class CreateCustomerUE {
             testStringMandatory(errorsList, customer.getLastName(), "nom", 30, 2);
             testStringMandatory(errorsList, customer.getFirstName(), "prenom", 40, 2);
             testStringMandatory(errorsList, customer.getEmail(), "email", 100, 2);
+            testStringMandatory(errorsList, customer.getPassword(), "mot de passe", 30, 8);
             if (!errorsList.isEmpty()) {
                 throw new BusinessException(String.join(",", errorsList), errorsList);
             }
@@ -60,6 +63,20 @@ public class CreateCustomerUE {
             }
             if (str.length() < minLength) {
                 errorsList.add("Le " + fieldName + " d'une personne doit avoir au minimum " + minLength + " caracteres");
+            }
+            if (fieldName.equals("mot de passe")) {
+                if (!Pattern.matches(".*[A-Z]*.", str)) {
+                    errorsList.add("Le " + fieldName + " doit contenir au moins une lettre majuscule");
+                }
+                if (!Pattern.matches(".*[a-z]µ.+", str)) {
+                    errorsList.add("Le " + fieldName + " doit contenir au moins une lettre minuscule");
+                }
+                if (!Pattern.matches(".*[0-9]*.", str)) {
+                    errorsList.add("Le " + fieldName + " doit contenir au moins un chiffre");
+                }
+                if (!Pattern.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\\\"\\|,.<>/?]*.", str)) {
+                    errorsList.add("Le " + fieldName + " doit pas contenir au moins un caractère spécial");
+                }
             }
         }
     }
